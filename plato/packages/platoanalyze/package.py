f07c2dadcbb982106134ba35e0edc6c496564c15
ralberd <ralberd@sandia.gov>
2023-10-16 11:22:45 -0600

    updating to ESP beta from 10/11

--- plato/packages/platoanalyze/package.py
+++ plato/packages/platoanalyze/package.py
@@ -83,7 +83,7 @@ class Platoanalyze(CMakePackage, CudaPackage):
 
     depends_on('arborx~mpi~cuda~serial @v1.1',              when='+meshmap')
     depends_on('amgx@2.2',                                  when='+amgx')
-    depends_on('esp@BetaLin-2023-07-17', type=('build', 'link', 'run'),        when='+esp')
+    depends_on('esp@BetaLin-2023-10-11', type=('build', 'link', 'run'),        when='+esp')
     depends_on('platoengine+esp',                           when='+esp')
     depends_on('numdiff',                                   when='+integration_tests')
     depends_on('py-numpy',                                  when='+dakota_tests')
