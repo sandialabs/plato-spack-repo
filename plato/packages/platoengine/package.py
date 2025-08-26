01ee0aa3a619fd5c05d24e8e0781c0ed45a34a3b
Adam Christopher Sokolow <acsokol@sandia.gov>
2025-08-26 01:13:00 +0000

    Reduces variants from removal of old PE

--- plato/packages/platoengine/package.py
+++ plato/packages/platoengine/package.py
@@ -18,68 +18,35 @@ class Platoengine(CMakePackage, CudaPackage):
     version('develop', branch='develop', preferred=True)
     version('release-v0.1.0', branch='release-v0.1.0')
 
-    variant( 'platomain',      default=True,    description='Compile PlatoMain'               )
+    
     variant( 'regression',     default=True,    description='Add regression tests'            )
     variant( 'unit_testing',   default=True,    description='Add unit testing'                )
     variant( 'esp',            default=False,   description='Turn on esp'                     )
-    variant( 'expy',           default=False,   description='Compile exodus/python API'       )
-    variant( 'iso',            default=False,   description='Turn on iso extraction'          )
-    variant( 'prune',          default=False,   description='Turn on use of prune and refine' )
-    variant( 'stk',            default=False,   description='Turn on use of stk'              )
-    variant( 'dakota',         default=False,   description='Compile with Dakota'             )
-    variant( 'services',       default=False,   description='Compile with services'           )
     variant( 'sierra_tests',   default=False,   description='Enable sierra testing'           )
     variant( 'dev_build',      default=False,   description='Build with dev features such as sanitizers and clang-tidy')
     variant( 'snopt',          default=False,   description='Build with SNOPT'                )
     variant( 'python',         default=False,   description='Build and link with python. This option is needed for plugins that depend on python')
-    variant( 'legacy',         default=True,    description='Build the original platoengine')
     variant( 'cubit',          default=False,   description='Build shape optimization geometry that uses Cubit library in prebuilt binaries'                )
     
-    conflicts( '+expy', when='-platomain')
-    conflicts( '+iso',  when='-stk')
-    conflicts( '+prune',  when='-stk')
-    conflicts( '@0.1.0', when='+prune')
-    conflicts( '@0.2.0', when='+prune')
-    conflicts( '@0.3.0', when='+prune')
-    conflicts( '@0.4.0', when='+prune')
-    conflicts( '@0.5.0', when='+prune')
-    conflicts( '@0.6.0', when='+prune')
-    conflicts( '+expy', when='+dakota')
-    conflicts( '~services', when='+dakota')
-    conflicts('~legacy', when='+prune', msg='prune requires legacy to be enabled')
-    conflicts('~legacy', when='+iso', msg='iso requires legacy to be enabled')
-    conflicts('~legacy', when='+platomain', msg='platomain requires legacy to be enabled')
-    conflicts('~legacy', when='+dakota', msg='dakota requires legacy to be enabled')
-    conflicts('~legacy', when='+services', msg='sevices requires legacy to be enabled')
-    conflicts('~legacy', when='+esp', msg='esp requires legacy to be enabled')
-    conflicts('~legacy', when='+expy', msg='expy requires legacy to be enabled')
-    conflicts('~legacy', when='+prune', msg='prune requires legacy to be enabled')
-    conflicts('~stk', when='~legacy', msg='Stk is required to build new platoengine')
-
     depends_on( 'mpi',            type=('build','link','run'))
     depends_on( 'cmake@3.0.0:',   type='build')
  
     trilinos_base_spec = 'trilinos@16.1.0+exodus+chaco+intrepid+shards+rol+tpetra~mumps'
     trilinos_base_add_ons = ' gotype=int cxxstd=17'
     depends_on( trilinos_base_spec + trilinos_base_add_ons)
-    depends_on( trilinos_base_spec + '+boost+krino+stk' + trilinos_base_add_ons, when='+stk')
-    depends_on( trilinos_base_spec + '+percept+zoltan+boost+stk' + trilinos_base_add_ons, when='+prune')
+    depends_on( trilinos_base_spec + '+boost+krino+stk+percept+zoltan' + trilinos_base_add_ons)
     depends_on( trilinos_base_spec + '+cuda+wrapper' + trilinos_base_add_ons, when='+cuda')
-    depends_on( trilinos_base_spec + '~cuda~krino~stk' + trilinos_base_add_ons, when='+dakota')
     depends_on( 'googletest',                                    when='+unit_testing' )
-    depends_on( 'python@3.8:',    type=('build', 'link', 'run'), when='+expy'    )
-    depends_on( 'nlopt',                                         when='+expy'         )
-    # py-setuptools later than v44.1.0 require python 3.x
-    depends_on( 'py-numpy',      when='+expy'         )
-
+    
     depends_on( 'esp@124Lin', type=('build', 'link', 'run'), when='+esp')
-    depends_on( 'dakota', when='+dakota')
     depends_on( 'numdiff', when='+regression')
     depends_on( 'boost+filesystem+serialization+system+program_options+regex+mpi')
 
     depends_on( 'snopt', when='+snopt')
 
     depends_on('python@3.8:', when='+python')
+    depends_on('python@3.8:', when='+regression')
+    depends_on('py-numpy',   when='+regression' )
     
     depends_on( 'llvm', when='+dev_build', type='build' )
 
@@ -104,21 +71,13 @@ class Platoengine(CMakePackage, CudaPackage):
         options.extend(
             [
                 self.define_from_variant("PLATOENGINE_ENABLE_CUDA","cuda"),
-                self.define_from_variant("PLATOMAIN","platomain"),
                 self.define_from_variant("UNIT_TESTING","unit_testing"),
-                self.define_from_variant("ENABLE_ISO","iso"),
-                self.define_from_variant("ENABLE_PRUNE","prune"),
-                self.define_from_variant("STK_ENABLED","stk"),
-                self.define_from_variant("ENABLE_PLATO_SERVICES","services"),
                 self.define_from_variant("SIERRA_TESTS_ENABLED","sierra_tests"),
-                self.define_from_variant("EXPY","expy"),
-                self.define_from_variant("PLATO_ENABLE_SERVICES_PYTHON","expy"),
                 self.define_from_variant("REGRESSION","regression"),
                 self.define_from_variant("SEACAS","regression"),
                 self.define_from_variant("BUILD_WITH_CLANG_TIDY","dev_build"),
                 self.define_from_variant("BUILD_WITH_SANITIZER_FLAGS","dev_build"),
                 self.define_from_variant("LINK_WITH_PYTHON","python"),
-                self.define_from_variant("LEGACY_BUILD","legacy"),
                 self.define_from_variant("CUBIT_ENABLED","cubit")
             ]
         )
@@ -144,21 +103,6 @@ class Platoengine(CMakePackage, CudaPackage):
               ]
             )
 
-        if '+dakota' in spec:
-          boost_dir = spec['boost'].prefix
-          options.extend(
-              [
-                self.define_from_variant("DAKOTADRIVER","dakota"),
-                '-DBOOST_ROOT:FILEPATH={0}'.format(boost_dir),
-                '-DCMAKE_CXX_COMPILER_VERSION={0}'.format(spec.compiler.version)  
-              ]
-            )  
-
-        if '+expy' in self.spec or '+python' in self.spec:
-          options.extend(
-             ['-DPython3_EXECUTABLE={0}/python3'.format(self.spec['python'].prefix.bin)]
-          )
-
         gcc_toolchain_flag = list(filter(lambda flag: "gcc-toolchain" in flag, spec.compiler_flags["cxxflags"]))
         if gcc_toolchain_flag:
           options.extend(['-DGCC_TOOLCHAIN_PATH={0}'.format(gcc_toolchain_flag[0].split('=')[-1])])
@@ -168,13 +112,7 @@ class Platoengine(CMakePackage, CudaPackage):
 
     def setup_run_environment(self, run_env):
         run_env.prepend_path('LD_LIBRARY_PATH', self.spec['platoengine'].prefix.lib)
-
-        if '+expy' in self.spec:
-          run_env.prepend_path('PYTHONPATH', self.prefix.lib)
-          run_env.prepend_path('PYTHONPATH', self.prefix.etc)
-
         run_env.prepend_path('LD_LIBRARY_PATH', self.spec['mpi'].prefix.lib)
-
         run_env.prepend_path('PATH', self.prefix.etc)
 
     def setup_build_environment(self, env):
