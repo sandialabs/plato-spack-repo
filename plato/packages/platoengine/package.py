0903a988b336743c18e3afd189ebc908a0622f83
ralberd <ralberd@sandia.gov>
2025-08-28 10:27:23 -0600

    [ci skip] changes name of trilinos version to be more descriptive

--- plato/packages/platoengine/package.py
+++ plato/packages/platoengine/package.py
@@ -31,7 +31,7 @@ class Platoengine(CMakePackage, CudaPackage):
     depends_on( 'mpi',            type=('build','link','run'))
     depends_on( 'cmake@3.0.0:',   type='build')
  
-    trilinos_base_spec = 'trilinos@develop_testing+exodus+chaco+intrepid+shards+rol+tpetra~mumps'
+    trilinos_base_spec = 'trilinos@16_0_1_krino_snapping+exodus+chaco+intrepid+shards+rol+tpetra~mumps'
     trilinos_base_add_ons = ' gotype=int cxxstd=17'
     depends_on( trilinos_base_spec + trilinos_base_add_ons)
     depends_on( trilinos_base_spec + '+boost+krino+stk+percept+zoltan' + trilinos_base_add_ons)
