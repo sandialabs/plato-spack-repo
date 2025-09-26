a373c5f16ecbeee99bb511b5f580eeeedebb5db1
Sean Hardesty <shardes@sandia.gov>
2025-09-26 16:54:39 -0600

    remove intrepid from PE trilinos spec

--- plato/packages/platoengine/package.py
+++ plato/packages/platoengine/package.py
@@ -31,7 +31,7 @@ class Platoengine(CMakePackage, CudaPackage):
     depends_on( 'mpi',            type=('build','link','run'))
     depends_on( 'cmake@3.0.0:',   type='build')
  
-    trilinos_base_spec = 'trilinos@16_1_0_update_krino_api+exodus+chaco+intrepid+shards+rol+tpetra~mumps'
+    trilinos_base_spec = 'trilinos@16_1_0_update_krino_api+exodus+chaco+shards+rol+tpetra~mumps'
     trilinos_base_add_ons = ' gotype=int cxxstd=17'
     depends_on( trilinos_base_spec + trilinos_base_add_ons)
     depends_on( trilinos_base_spec + '+boost+krino+stk+percept+zoltan' + trilinos_base_add_ons)
